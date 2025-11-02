7bac82aee20f8b31055b94d198ad8fb9b35688ab
Lakshya151 <lakshyarajputr@gmail.com>
2025-11-02 11:19:55 +0000

    conversion of dataTypes

---
+++ 01_basics/03_ConversionOperator.js
@@ -0,0 +1,24 @@
+let score ="33"// here I define a String.
+console.log(typeof(score))
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber)) 
+//if there was 33ab , instead of number , then the dataType will be the number , but the value will be Nan
+let score_2="33ab"
+let NanScore=Number(score_2)
+console.log(typeof(score_2))// type will be number
+console.log(NanScore) // but ans will be NaN
+/*if inside score the value was null , then after convert to number , the ans will be 0.
+and if value was undefined then ans of number will be NaN.
+And true =1 and false =0
+*/
+
+let isLoggedIn= 1
+let boolIsLoggedIn= Boolean(isLoggedIn)
+console.log(boolIsLoggedIn)// the ans will be true.
+
+// Now number to string convert
+
+let num=1234
+let stringNum= String(num)
+console.log(typeof(stringNum))
+console.log(stringNum)
