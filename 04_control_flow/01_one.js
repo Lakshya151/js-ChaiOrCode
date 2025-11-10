1ae5454d49dc702c0c4d5134256bab54a40365d7
Lakshya151 <lakshyarajputr@gmail.com>
2025-11-10 00:37:42 +0000

    if- else statement info and 02_switch statementm info

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,9 @@
+//if
+//operators -> < ,> ,>= ,<=,=, == ,!= ,=== (this will check value and datatype both) ,!==
+
+// IF else if , else same as in cpp ,  && and || also same
+if(2==="2"){
+    console.log("they are equal.")
+}else{
+    console.log("they are not equal.")
+}
