33c360fc1a0137233c322ad79dbfeecc8c05de9e
Lakshya151 <lakshyarajputr@gmail.com>
2025-11-10 01:00:21 +0000

    switch and another way to use if-else

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,30 @@
+//----------Basic Syntax-----------
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month =3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    default:
+        console.log("left month");
+        break;
+}
+
+//-------if-else--------
+const cost=100;
+(cost >=80) ?console.log("yes ! cost is greater than 80 .") :console.log("No ! cost isn't greater then 80.")
