999c7cb455d4af6939e1a262c422152ca8ad384f
Lakshya151 <lakshyarajputr@gmail.com>
2025-11-16 16:45:19 +0000

    just comment some line in 05 in 01 basics

--- 01_basics/05_String.js
+++ 01_basics/05_String.js
@@ -3,16 +3,16 @@
 const name="Lakshya"
 const repoCount=3
 //console.log(name+repoCount) // not much good
-console.log(`My name is ${name} and my repocount is ${repoCount}.`)
+//console.log(`My name is ${name} and my repocount is ${repoCount}.`)
 
 //Another method to define string->// with this type , i can have access of many methods in insept console
 const gameName= new String("PUBG")
-console.log(gameName[0])// one method from many
+//console.log(gameName[0])// one method from many
 console.log(gameName.__proto__)// string method 
 console.log(gameName.length)   // string method
-console.log(gameName.toUpperCase())//string method     
-console.log(gameName.charAt(2))
-console.log(gameName.indexOf('U'))
+//console.log(gameName.toUpperCase())//string method     
+//console.log(gameName.charAt(2))
+//console.log(gameName.indexOf('U'))
 
 
 const newString = name.substring(0,4)// can't use negative index here
@@ -27,7 +27,7 @@ console.log(newStr.trim())// it will trim the space here.
 
 const url="https://Lakshya95.com"
 console.log(url.replace('95','-rajput'))// this will replace the given text.
-console.log(url.includes('rajput'))// this will give true ot false , if not present in url
+console.log(url.includes('rajput'))// this will give true or false , if not present in url
 /* this will give false , as url is primitive type , so oroginal value haven't change 
 and there is no rajput in it*/
 
