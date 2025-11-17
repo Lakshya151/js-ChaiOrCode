c57aefb033282464d45334a2b65e3a51320afc74
Lakshya151 <lakshyarajputr@gmail.com>
2025-11-17 12:50:29 +0000

    fix some logical error

--- 07_projects/02_BMI_Generator/jsFile.js
+++ 07_projects/02_BMI_Generator/jsFile.js
@@ -1,13 +1,13 @@
 const form = document.querySelector('form')
 form.addEventListener('submit',function(e){
-    e.preventDefault()
+    e.preventDefault();// as we don't hava DB ,so we stop submitting the result to DB.
     const height=parseInt(document.querySelector('#height').value) // by .value we get the input value  , but in format of string , so we have to convert it into number or any required data type.
     const weight=parseInt(document.querySelector('#weight').value)
-    const result=document.querySelector('#result')
+    const result=document.querySelector('#results')
     if(height===''||height<0 || isNaN(height)) {// this NaN will check if the given height is a number or not
         result.innerHTML=`please give a valid height : ${height}`;
     }else if(weight===''||weight<0 || isNaN(weight)) {
-        result.innerHTML=`please give a valid height : ${weight}`;
+        result.innerHTML=`please give a valid weight : ${weight}`;
     }else{
         const bmi=(weight/((height*height)/10000)).toFixed(2);
         //show the result 
