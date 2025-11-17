3ff6117bd5c27a40b207bc9336595bad9e61669f
Lakshya151 <lakshyarajputr@gmail.com>
2025-11-17 12:41:08 +0000

    js file of bmi generator , project 2

---
+++ 07_projects/02_BMI_Generator/jsFile.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('form')
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+    const height=parseInt(document.querySelector('#height').value) // by .value we get the input value  , but in format of string , so we have to convert it into number or any required data type.
+    const weight=parseInt(document.querySelector('#weight').value)
+    const result=document.querySelector('#result')
+    if(height===''||height<0 || isNaN(height)) {// this NaN will check if the given height is a number or not
+        result.innerHTML=`please give a valid height : ${height}`;
+    }else if(weight===''||weight<0 || isNaN(weight)) {
+        result.innerHTML=`please give a valid height : ${weight}`;
+    }else{
+        const bmi=(weight/((height*height)/10000)).toFixed(2);
+        //show the result 
+        result.innerHTML=`<span>${bmi}</span>`;
+    }
+});
