5af6203fdfeb2a5f8a613b7a23c953e2718e094d
Lakshya151 <lakshyarajputr@gmail.com>
2025-11-17 07:54:52 +0000

    .

--- 07_projects/01_color_changer/jsFile.js
+++ 07_projects/01_color_changer/jsFile.js
@@ -1,4 +1,5 @@
 const button =document.querySelectorAll('.button')
+const body = document.querySelector("body");
 button.forEach(function(button){
     console.log(button)
     button.addEventListener('click',function(e){
