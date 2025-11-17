00fd51e8e3e30ae8ea27101e5e1361b17c5954ba
Lakshya151 <lakshyarajputr@gmail.com>
2025-11-17 08:08:17 +0000

    replace color name to , to color of the box itself by giving their id

--- 07_projects/01_color_changer/jsFile.js
+++ 07_projects/01_color_changer/jsFile.js
@@ -1,22 +1,22 @@
 const button =document.querySelectorAll('.button')
 const body = document.querySelector("body");
 button.forEach(function(button){
-    console.log(button)
+    console.log(button)// this will show all the buttons on console and their details
     button.addEventListener('click',function(e){
         console.log(e);
-        console.log(e.target);
-        switch(e.target.id){
+        console.log(e.target);// this will show the details of the clicked button
+        switch(e.target.id){ // this e.target will give the box that we click on , and then .id will provide its id , which is given on div in html file.
             case 'grey':
-                body.style.background="grey";
+                body.style.background=e.target.id;
                 break;
             case 'white':
-                body.style.background="white";
+                body.style.background=e.target.id;
                 break;
             case 'blue':
-                body.style.background="blue";
+                body.style.background=e.target.id;
                 break;
             case 'yellow':
-                body.style.background="yellow";
+                body.style.background=e.target.id;
                 break;
             default:
                 body.style.background="purple";
